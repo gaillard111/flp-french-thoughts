1ced3a8cd5e9d457b51e069da24e1c045b4dfbcc
ars <arseny.k@zimalab.com>
2018-08-27 11:20:41 +0700

    fixes

--- src/ThoughtBundle/Controller/UserProfileController.php
+++ src/ThoughtBundle/Controller/UserProfileController.php
@@ -29,7 +29,7 @@ class UserProfileController extends Controller
         );
 
 
-        return $this->render('@Thought/userList.html.twig', [
+        return $this->render('usersList.html.twig', [
             'users' =>  $pagination,
         ]);
     }
