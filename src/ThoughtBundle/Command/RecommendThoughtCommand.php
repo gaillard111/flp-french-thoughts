b722425a2da963b6f392073243a3259410bc2278
ars <arseny.k@zimalab.com>
2019-07-22 14:02:05 +0700

    Recommended thought command (fix)

--- src/ThoughtBundle/Command/RecommendThoughtCommand.php
+++ src/ThoughtBundle/Command/RecommendThoughtCommand.php
@@ -48,7 +48,7 @@ class RecommendThoughtCommand extends ContainerAwareCommand
                     $recommendedThought = array_shift($thA);
                     $recommendedThoughtService->addWatchedThought($user, $recommendedThought);
                 } catch (\Exception $e) {
-                    dump($recommendedThought->getContent());
+                    dump($recommendedThought);
                     continue;
                 }
 
