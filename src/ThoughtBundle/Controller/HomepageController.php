cd4b7ea77cc6e3213efe2a00261b46fd558cced3
ars <arseny.k@zimalab.com>
2019-08-20 15:52:35 +0700

    Tutorial banners (fix)

--- src/ThoughtBundle/Controller/HomepageController.php
+++ src/ThoughtBundle/Controller/HomepageController.php
@@ -122,8 +122,7 @@ class HomepageController extends Controller
             $response->headers->setCookie(new Cookie('modal', true, $time));
         }
 
-//        dump($pagination->getTotalItemCount() > 0);
-        if ($pagination->getTotalItemCount() > 0) {
+        if (($pagination->getTotalItemCount() > 0) && $search) {
             if ((!$request->cookies->get('comment_modal')) && ($request->cookies->get('modal'))) {
                 $time = time() + (3600 * 24 * 7);
                 $response->headers->setCookie(new Cookie('comment_modal', true, $time));
