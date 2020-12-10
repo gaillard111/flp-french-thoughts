ad23fed2bc33df9cabadb5f1db61c9b5aea7880a
Nikita Lukanin <lukanin.nikita@yandex.ru>
2020-12-10 14:16:26 +0700

    Removed search limit

--- src/ThoughtBundle/Model/ThoughtModel.php
+++ src/ThoughtBundle/Model/ThoughtModel.php
@@ -1068,7 +1068,7 @@ class ThoughtModel
             ],
         ]);
 
-        $authors = $this->authorsFinder->find($query, 5000);
+        $authors = $this->authorsFinder->find($query);
 
         $names = [];
 
